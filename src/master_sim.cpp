b6c2f58b55c20fa1aca30ec64ca07ce26cdd2f32
Hoang Trung Huy <huyht1205@gmail.com>
2016-06-29 14:42:40 +0700

    master_sim: add line tracking subscriber
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- src/master_sim.cpp
+++ src/master_sim.cpp
@@ -1,19 +1,22 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/UInt32.h"
 #include "sensor_msgs/Range.h"
 
-void sensorCallBack(const sensor_msgs::Range::ConstPtr& msg)
-{
-        ROS_INFO("I heard: [%.2f]", msg->range);
+void range_sensor_callback(const sensor_msgs::Range::ConstPtr& msg) {
+        ROS_INFO("Range: [%.2f] mm", msg->range);
 
 }
 
-int main(int argc, char **argv)
-{
+void lineTracking_sensor_callback(const std_msgs::UInt32::ConstPtr& msg) {
+        ROS_INFO("Line Tracking value: [%d]", msg->data);
+}
+
+int main(int argc, char **argv) {
         ros::init(argc, argv, "master");
 
         ros::NodeHandle masterNode;
-        ros::Subscriber range_sub = masterNode.subscribe("/was_sensor/range/ultrasound", 1000, sensorCallBack);
+        ros::Subscriber range_sub = masterNode.subscribe("/was_sensor/range/ultrasound", 1000, range_sensor_callback);
+        ros::Subscriber lineTracking_sub = masterNode.subscribe("/was_sensor/movement/lineTracking", 1000, lineTracking_sensor_callback);
 
         ros::spin();
         

b159f6a4d4e7db24542eb69500fc04e48df4dff1
Hoang Trung Huy <huyht1205@gmail.com>
2016-07-13 10:54:10 +0700

    master_sim: change to class-based design

--- src/master_sim.cpp
+++ src/master_sim.cpp
@@ -11,12 +11,26 @@ void lineTracking_sensor_callback(const std_msgs::UInt32::ConstPtr& msg) {
         ROS_INFO("Line Tracking value: [%d]", msg->data);
 }
 
+class was_master{
+private:
+        ros::NodeHandle nh;
+        ros::Subscriber range_sub;
+        ros::Subscriber lineTracking_sub;
+
+public:
+        was_master() {
+                range_sub = nh.subscribe("/was_sensor/range/ultrasound",
+                                        1000, range_sensor_callback);
+                lineTracking_sub = nh.subscribe("/was_sensor/movement/lineTracking",
+                                        1000, lineTracking_sensor_callback);
+        }
+
+};
+
 int main(int argc, char **argv) {
         ros::init(argc, argv, "master");
 
-        ros::NodeHandle masterNode;
-        ros::Subscriber range_sub = masterNode.subscribe("/was_sensor/range/ultrasound", 1000, range_sensor_callback);
-        ros::Subscriber lineTracking_sub = masterNode.subscribe("/was_sensor/movement/lineTracking", 1000, lineTracking_sensor_callback);
+        was_master wasMaster = was_master();
 
         ros::spin();
         

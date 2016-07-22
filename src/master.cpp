aa8ae38efab41eefde1e30f47d5fbcb1c29b7439
Hoang Trung Huy <huyht1205@gmail.com>
2016-07-22 15:15:51 +0700

    update
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- src/master.cpp
+++ src/master.cpp
@@ -1,10 +1,10 @@
 #include "master.h"
 
 void was_master::range_cb(const sensor_msgs::Range::ConstPtr& msg) {
-        ROS_INFO("Range: [%.2f] mm", msg->range);
+        ROS_DEBUG("Range: [%.2f] mm", msg->range);
 
 }
 
 void was_master::lineTracking_cb(const std_msgs::UInt32::ConstPtr& msg) {
-        ROS_INFO("Line Tracking value: [%d]", msg->data);
+        ROS_DEBUG("Line Tracking value: [%d]", msg->data);
 }

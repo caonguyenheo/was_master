dc99587212ed926c4d12b1e6222c88267a3baa70
Hoang Trung Huy <huyht1205@gmail.com>
2016-07-27 16:13:26 +0700

    master: add teleop
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- include/master.h
+++ include/master.h
@@ -1,15 +1,17 @@
 #include "ros/ros.h"
 #include "std_msgs/UInt32.h"
 #include "sensor_msgs/Range.h"
+#include "std_msgs/String.h"
 
 class was_master{
 private:
         ros::NodeHandle nh;
         ros::Subscriber range_sub;
         ros::Subscriber lineTracking_sub;
+        ros::Subscriber teleop_sub;
         void range_cb(const sensor_msgs::Range::ConstPtr& msg);
         void lineTracking_cb(const std_msgs::UInt32::ConstPtr& msg);
-
+        void teleop_cb(const std_msgs::String::ConstPtr& cmd);
 
 public:
         was_master() {
@@ -17,6 +19,8 @@ public:
                                          1000, &was_master::range_cb, this);
                 lineTracking_sub = nh.subscribe("/was_sensor/movement/lineTracking",
                                                 1000, &was_master::lineTracking_cb, this);
+                teleop_sub = nh.subscribe("was_control/moving",
+                                        1000, &was_master::teleop_cb, this);
         }
         
 };
--- src/master.cpp
+++ src/master.cpp
@@ -2,9 +2,12 @@
 
 void was_master::range_cb(const sensor_msgs::Range::ConstPtr& msg) {
         ROS_DEBUG("Range: [%.2f] mm", msg->range);
-
 }
 
 void was_master::lineTracking_cb(const std_msgs::UInt32::ConstPtr& msg) {
         ROS_DEBUG("Line Tracking value: [%d]", msg->data);
+}
+
+void was_master::teleop_cb(const std_msgs::String::ConstPtr& cmd) {
+        std::cout << cmd->data << std::endl;
 }
